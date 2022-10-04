927ec59aa35f6e0f1569449633fcf4ca5e57333a
jemmiesalami <jemimahsalami@gmail.com>
2022-10-03 22:26:55 -0700

    more and more pointers, arrays, and strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,30 +2,28 @@
 #include <stdio.h>
 
 /**
- * _strstr -  a function that locates a substring.
- * @haystack: an input string to search in
- * @needle: an input string to locate into string haystack
- * Return:  a pointer to the beginning of the located substring,
- * or NULL if the substring is not found.
+ * _strstr -  a function that locates a substring
+ * @needle: Occurance to find
+ * @haystack: String to search
+ * Return:  pointer
  */
 
 char *_strstr(char *haystack, char *needle)
 {
-	char *startn = needle, *starth = haystack;
-
 	while (*haystack)
 	{
-		starth = haystack;
-		needle = startn;
-		while (*haystack == *needle)
+		char *src = haystack;
+		char *sub = needle;
+
+		while (*haystack && *sub && *haystack == *sub)
 		{
 			haystack++;
-			needle++;
+			sub++;
 		}
 
-		if (*needle == '\0')
-			return (haystack);
-		haystack = starth + 1;
+		if (!*sub)
+			return (src);
+		haystack = src + 1;
 	}
-	return (NULL);
+	return (0);
 }
