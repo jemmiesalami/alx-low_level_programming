975257401bf4cf19aac3777e9dd123fd09ea1183
jemmiesalami <jemimahsalami@gmail.com>
2022-10-07 21:52:25 -0700

    more memory allocation files

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -14,19 +14,15 @@ int main(int argc, char *argv[])
 unsigned long mul;
 int i, j;
 	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+	{ printf("Error\n");
+	exit(98); }
 	for (i = 1; i < argc; i++)
 	{
 		for (i = 1; i < argc; i++)
 		{
 			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{
-				printf("Error\n");
-				exit(98);
-			}
+			{ printf("Error\n");
+			exit(98); }
 		}
 
 	}
