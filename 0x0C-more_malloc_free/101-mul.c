6fc113e1e2c3b0b2723fb3a3973e3e03b65f64b7
jemmiesalami <jemimahsalami@gmail.com>
2022-10-10 21:23:43 -0700

    more memory allocation files

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -18,7 +18,7 @@ int i, j;
 	exit(98); }
 	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (argv[i][j] > 57 || argv[i][j] < 48)
 			{ printf("Error\n");
